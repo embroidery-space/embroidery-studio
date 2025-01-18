bbff9a3d9737a4a7d5c40135d26b2ea0c11a7667
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-01-18 09:07:31 +0200

    fix: embed custom Windows application manifest on build to prevent errors in tests (#75)

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,25 @@
 fn main() {
-  tauri_build::build()
+  #[cfg(all(target_os = "windows", target_env = "msvc", feature = "test"))]
+  {
+    use tauri_build::WindowsAttributes;
+
+    let attributes = tauri_build::Attributes::new().windows_attributes(WindowsAttributes::new_without_app_manifest());
+    tauri_build::try_build(attributes).expect("failed to run tauri-build");
+
+    let manifest = std::env::current_dir()
+      .unwrap()
+      .join("testdata/")
+      .join("windows-app-manifest.xml");
+
+    // Workaround needed to prevent `STATUS_ENTRYPOINT_NOT_FOUND` error in tests on Windows.
+    // See https://github.com/tauri-apps/tauri/discussions/11179.
+    println!("cargo:rerun-if-changed={}", manifest.display());
+    println!("cargo:rustc-link-arg=/MANIFEST:EMBED"); // Embed the Windows application manifest file.
+    println!("cargo:rustc-link-arg=/MANIFESTINPUT:{}", manifest.to_str().unwrap());
+    println!("cargo:rustc-link-arg=/WX"); // Turn linker warnings into errors.
+  }
+
+  // Keep the default build script for other cases.
+  #[cfg(not(all(target_os = "windows", target_env = "msvc", feature = "test")))]
+  tauri_build::build();
 }
