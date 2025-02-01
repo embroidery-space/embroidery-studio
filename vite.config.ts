86dcde1c0156d132263609d5fa10af620e7d45d1
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-02-01 09:42:40 +0200

    update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,16 @@ export default defineConfig({
   plugins: [vue(), tailwindcss()],
   clearScreen: false,
   resolve: { alias: { "#": fileURLToPath(new URL("./src", import.meta.url)) } },
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
   server: {
     port: 1420,
     strictPort: true,
     watch: { ignored: ["src-tauri/**"] },
+  },
+  build: {
+    target: process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
   test: { environment: "jsdom", globals: true },
 });
