148c535720a9ce64267802d5a466e3e767d8ca04
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-02-12 09:55:59 +0200

    extract a texture creation logic into a separate method

--- src/plugins/pixi/texture-manager.ts
+++ src/plugins/pixi/texture-manager.ts
@@ -1,4 +1,5 @@
-import { Graphics, Matrix, RenderTexture, type Renderer, type TextureSourceOptions } from "pixi.js";
+import { Container, Graphics, Matrix, RenderTexture } from "pixi.js";
+import type { Renderer, RenderOptions, TextureSourceOptions } from "pixi.js";
 import { TEXTURE_STROKE } from "./constants";
 import { ObjectedMap } from "#/utils/map";
 import { mm2px } from "#/utils/measurement";
@@ -39,18 +40,12 @@ export class TextureManager {
   #createFullStitchTextures() {
     return {
       [FullStitchKind.Full]: (() => {
-        const rt = RenderTexture.create(Object.assign({ width: 100, height: 100 }, this.#textureSourceOptions));
         const shape = new Graphics().rect(0, 0, 100, 100).fill(0xffffff);
-        this.#renderer.render({ container: shape, target: rt });
-        shape.destroy(true);
-        return rt;
+        return this.#createTexture(shape, { width: 100, height: 100 });
       })(),
       [FullStitchKind.Petite]: (() => {
-        const rt = RenderTexture.create(Object.assign({ width: 50, height: 50 }, this.#textureSourceOptions));
         const shape = new Graphics().rect(1, 1, 48, 48).stroke(TEXTURE_STROKE).fill(0xffffff);
-        this.#renderer.render({ container: shape, target: rt });
-        shape.destroy(true);
-        return rt;
+        return this.#createTexture(shape, { width: 50, height: 50 });
       })(),
     };
   }
@@ -62,24 +57,18 @@ export class TextureManager {
   #createPartStitchTextures() {
     return {
       [PartStitchKind.Half]: (() => {
-        const rt = RenderTexture.create(Object.assign({ width: 100, height: 100 }, this.#textureSourceOptions));
         const shape = new Graphics()
           .poly([99, 1, 99, 35, 35, 99, 1, 99, 1, 65, 65, 1])
           .stroke(TEXTURE_STROKE)
           .fill(0xffffff);
-        this.#renderer.render({ container: shape, target: rt });
-        shape.destroy(true);
-        return rt;
+        return this.#createTexture(shape, { width: 100, height: 100 });
       })(),
       [PartStitchKind.Quarter]: (() => {
-        const rt = RenderTexture.create(Object.assign({ width: 50, height: 50 }, this.#textureSourceOptions));
         const shape = new Graphics()
           .poly([49, 1, 49, 25, 25, 49, 1, 49, 1, 25, 25, 1])
           .stroke(TEXTURE_STROKE)
           .fill(0xffffff);
-        this.#renderer.render({ container: shape, target: rt });
-        shape.destroy(true);
-        return rt;
+        return this.#createTexture(shape, { width: 50, height: 50 });
       })(),
     };
   }
@@ -92,23 +81,28 @@ export class TextureManager {
   }
 
   #createFrenchKnotTexture() {
-    const rt = RenderTexture.create(Object.assign({ width: 50, height: 50 }, this.#textureSourceOptions));
     const shape = new Graphics().circle(0, 0, 24).stroke(TEXTURE_STROKE).fill(0xffffff);
-    this.#renderer.render({ container: shape, target: rt, transform: new Matrix(1, 0, 0, 1, 25, 25) });
-    shape.destroy(true);
-    return rt;
+    return this.#createTexture(shape, { width: 50, height: 50 }, { transform: new Matrix(1, 0, 0, 1, 25, 25) });
   }
 
   #createBeadTexture(bead: Bead) {
     const width = mm2px(bead.diameter) * 10;
     const height = mm2px(bead.length) * 10;
-    const rt = RenderTexture.create(Object.assign({ width, height }, this.#textureSourceOptions));
     const shape = new Graphics()
       .roundRect(1, 2, width - 2, height - 4, (width - 2) * 0.4)
       .stroke(TEXTURE_STROKE)
       .fill(0xffffff);
-    this.#renderer.render({ container: shape, target: rt });
-    shape.destroy(true);
+    return this.#createTexture(shape, { width, height });
+  }
+
+  #createTexture(
+    container: Container,
+    textureSourceOptions?: Partial<TextureSourceOptions>,
+    renderOptions?: Omit<RenderOptions, "container" | "target">,
+  ) {
+    const rt = RenderTexture.create(Object.assign({}, this.#textureSourceOptions, textureSourceOptions));
+    this.#renderer.render({ container, target: rt, ...renderOptions });
+    container.destroy(true);
     return rt;
   }
 }
