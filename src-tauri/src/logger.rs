07633ecd9f63ddb11f67d6bf831e3dd04e127329
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-04-11 12:21:14 +0300

    log panics

--- src-tauri/src/logger.rs
+++ src-tauri/src/logger.rs
@@ -1,23 +1,28 @@
-use std::str::FromStr;
+use std::str::FromStr as _;
 
-use log::Level;
 use tauri_plugin_log::{Target, TargetKind};
 
-pub fn setup_logger() -> tauri_plugin_log::Builder {
+pub fn init<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
+  log_panics::init();
+
   let log_level = std::env::var("RUST_LOG").unwrap_or(String::from("INFO"));
-  let log_level = Level::from_str(&log_level).unwrap_or(Level::Info);
-  let max_file_size = match log_level {
-    Level::Error | Level::Warn | Level::Info => 48 * 1024, // 48 KiB
-    _ => 256 * 1024,                                       // 256 KiB
-  };
+  let log_level = log::Level::from_str(&log_level).unwrap();
+
   tauri_plugin_log::Builder::default()
     .clear_targets()
     .targets([
+      // In debug mode, log to `stderr` and to `src-tauri/logs/debug.log` file.
       #[cfg(debug_assertions)]
       Target::new(TargetKind::Stderr),
+      #[cfg(debug_assertions)]
+      Target::new(TargetKind::Folder {
+        path: std::path::PathBuf::from("logs"),
+        file_name: Some(String::from("debug")),
+      }),
+      // In release mode, log to an application log dir.
       #[cfg(not(debug_assertions))]
       Target::new(TargetKind::LogDir { file_name: None }),
     ])
-    .level_for("embroidery_studio", log_level.to_level_filter())
-    .max_file_size(max_file_size)
+    .level(log_level.to_level_filter())
+    .build()
 }
