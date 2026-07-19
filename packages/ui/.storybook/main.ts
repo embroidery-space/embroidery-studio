a8af1ae2ffb41e9284d8e38395c222c44cfd2758
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-07-19 12:14:20 +0300

    remove `*.mdx` matcher

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 export default {
   staticDirs: ["../public"],
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.ts"],
+  stories: ["../src/**/*.stories.ts"],
   addons: ["@storybook/addon-docs", "@storybook/addon-vitest", "storybook-addon-vis"],
   framework: {
     name: "@storybook/vue3-vite",
